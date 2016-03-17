4e0c95d2d0909f85824df7517fdfecca1c7968e6
Jordan Leeper <j.m.leep@gmail.com>
2016-03-16 21:29:09 -0500

    Added Facebook login functionality to main view controller

--- StudyTable/ViewController.swift
+++ StudyTable/ViewController.swift
@@ -7,12 +7,20 @@
 //
 
 import UIKit
+import FBSDKLoginKit
+import FBSDKShareKit
+import FBSDKCoreKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, FBSDKLoginButtonDelegate {
 
+    @IBOutlet weak var name: UILabel!
+    @IBOutlet weak var fbButton: FBSDKLoginButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        name.text = "test"
+        fbButton.readPermissions = ["public_profile",  "email", "user_friends"]
+        fbButton.delegate = self
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +28,21 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    func loginButton(loginButton: FBSDKLoginButton!, didCompleteWithResult result: FBSDKLoginManagerLoginResult!, error: NSError!) {
+        FBSDKGraphRequest.init(graphPath: "me", parameters: ["fields":"first_name, last_name, picture.type(large)"]).startWithCompletionHandler { (connection, result, error) -> Void in
+            let strFirstName: String = (result.objectForKey("first_name") as? String)!
+            let strLastName: String = (result.objectForKey("last_name") as? String)!
+            
+            self.name.text = "Welcome, \(strFirstName) \(strLastName)"
+            }
+    }
+    
+    func loginButtonDidLogOut(loginButton: FBSDKLoginButton!) {
+        let loginManager: FBSDKLoginManager = FBSDKLoginManager.init()
+        loginManager.logOut()
+        
+        name.text = ""
+    }
 
 }
 
